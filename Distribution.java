871925809fad349ca3e69e66a1ef3c980986ed74
Evert <weball1@catamount.wcu.edu>
2019-05-02 15:36:35 -0400

    Finished Distribution

--- Distribution.java
+++ Distribution.java
@@ -1,3 +1,7 @@
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.ArrayList;
+
 /**
  * The Driver of the program. Confirms that all command line input exists 
  * and is correct, then determines which direction to take based on above
@@ -20,19 +24,14 @@ public class Distribution {
      */
     public static void main(String[] args) {
         
-        System.out.println("\u001b[43m"); //bg yellow
-        System.out.println("\u001b[30m"); //text black
-        System.out.println("Class Under Construction");
-        System.out.println("\u001b[0m"); //reset colors.
-
-        if(args.length != 3) {
+        if(args.length != 2) {
             System.out.println("Usage is: java Distribution <time_to_run> <T|F>");
             System.exit(1);
         }
 
-        timeToRun = Integer.parseInt(args[1]);
+        timeToRun = Integer.parseInt(args[0]);
 
-        if(args[2].toLowerCase() == "t") {
+        if(args[1].toLowerCase() == "t") {
             
             //TODO print output to log.txt
             
@@ -40,17 +39,79 @@ public class Distribution {
             if(timeToRun <= 0) {
                 System.out.println("Time must be positive to output to a file");
                 System.exit(1);
+            } else {
+                PrintStream out;
+                try {
+                    out = new PrintStream("./log.txt");
+                    System.setOut(out);
+                } catch (FileNotFoundException fnfe) {
+                    System.out.println("File Could Not Be Found or Created...");
+                    System.exit(1);
+                }
             }
         } 
         else {
-            //TODO print output to stdout
 
             if(timeToRun <= 0) {
-                //TODO run forever (until ctrl + c)
+                timeToRun = 0;
             }
         } // end outter-most if-else
 
+        Distribution distributor = new Distribution();
+        distributor.distribute();
+
 
     } // end main()
+
+    /**
+     * What actually runs our program.
+     */
+    private void distribute() {
+
+        ArrayList<Miners> miners = new ArrayList<>();
+        ArrayList<Messenger> messengers = new ArrayList<>();
+
+        //Create the dock and the foreman.
+        Docks dock = new Docks();
+        Foreman foreman = new Foreman(dock);
+
+        // Create the three types of miners.
+        Miners cheeseMiner = new Miners("cheese", dock);
+        Miners breadMiner = new Miners("bread", dock);
+        Miners bolognaMiner = new Miners("bologna", dock);
+
+        miners.add(cheeseMiner);
+        miners.add(breadMiner);
+        miners.add(bolognaMiner);
+
+        // Create the three messenger types.
+        Messenger cheeseMessenger = new Messenger("cheese", dock);
+        Messenger breadMessenger = new Messenger("bread", dock);
+        Messenger bolognaMessenger = new Messenger("bologna", dock);
+
+        messengers.add(cheeseMessenger);
+        messengers.add(breadMessenger);
+        messengers.add(bolognaMessenger);
+
+        //Starts the foreman thread.
+        foreman.run();
+
+        //Starts the each type of miner thread.
+        for (Miners miner : miners) {
+            miner.run();
+        }
+
+        //Starts the each type of messenger thread.
+        for (Messenger messenger : messengers) {
+            messenger.run();
+        }
+
+        try {
+            Thread.currentThread().join(timeToRun);
+        } catch(InterruptedException ie) {
+            ie.printStackTrace();
+        }
+
+    }
 
 }
