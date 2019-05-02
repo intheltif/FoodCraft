8f87d6805f9d07cfe19f4eb777c25f59fb407ce9
Evert <weball1@catamount.wcu.edu>
2019-05-02 11:57:04 -0400

    Updated documentation and added constructors and comments

--- Distribution.java
+++ Distribution.java
@@ -1,11 +1,56 @@
-
+/**
+ * The Driver of the program. Confirms that all command line input exists 
+ * and is correct, then determines which direction to take based on above
+ * mentioned command line input.
+ *
+ * @author Evert Ball
+ * @author Donny Queen
+ *
+ * @version 05/03/2019
+ */
 public class Distribution {
     
+    /** The time to run the program */
+    private int timeToRun;
+    
+    /**
+     * The main entry point to the program
+     *
+     * @param args The command line arguments
+     */
     public static void main(String[] args) {
+        
         System.out.println("\u001b[43m"); //bg yellow
         System.out.println("\u001b[30m"); //text black
         System.out.println("Class Under Construction");
         System.out.println("\u001b[0m"); //reset colors.
-    }
+
+        if(args.length < 3) {
+            System.out.println("Usage is: java Distribution <time_to_run> <T|F>");
+            System.exit(1);
+        }
+
+        this.timeToRun = Integer.parseInt(args[1]);
+
+        if(args[2].toLowerCase() == "t") {
+            
+            //TODO print output to log.txt
+            
+            //Time must be positive to write to file.
+            if(this.timeToRun < 0) {
+                System.out.println("Time must be positive to output to a file");
+                System.exit(1);
+            }
+        } 
+        else {
+            //TODO print output to stdout
+
+            if(timeToRun <= 0) {
+                //TODO run forever (until ctrl + c)
+            }
+        } // end outter-most if-else
+
+
+    } // end main()
 
 }
--- Messenger.java
+++ Messenger.java
@@ -1,3 +1,21 @@
+/**
+ * The messenger class that sends and receives signals from processes.
+ *
+ * @author Evert Ball
+ * @author Donny Queen
+ *
+ * @version 05/03/2019
+ */
 public class Messenger {
+    
+    /**
+     * Constructs a messenger object that holds the semaphores.
+     * TODO Might need to by synchronized and static?
+     */
+    public Messenger() {
+
+        //Create semaphores
+
+    } // end constructor
 
 } // end Messenger
